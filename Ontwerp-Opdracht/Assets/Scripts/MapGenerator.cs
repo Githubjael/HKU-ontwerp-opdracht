9f4f15194668603ea4001f7c01777e133cb35652
Githubjael <jaelimamdi@gmail.com>
2022-01-02 18:34:45 +0100

    MapGenerator script
    een script om mappen te generen. een script om mappen te displayen moet nog gemaakt worden.

---
+++ Ontwerp-Opdracht/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public void GenerateMap()
+    {
+
+    }
+}
