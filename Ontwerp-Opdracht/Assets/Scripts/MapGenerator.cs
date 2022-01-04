b840083e200015c78c7ac67357afcf13f0cb09a6
Githubjael <jaelimamdi@gmail.com>
2022-01-04 19:36:09 +0100

    MapGenerator script geupdate
    colormap en en drawmode erbij gevoegd.

--- Ontwerp-Opdracht/Assets/Scripts/MapGenerator.cs
+++ Ontwerp-Opdracht/Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode {noiseMap, colorMap }
+    public DrawMode drawMode;
+    
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -18,12 +21,39 @@ public class MapGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+
+        for(int y = octaves; y < mapHeight; y++) 
+        { 
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            } 
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if(drawMode == DrawMode.noiseMap)
+        {
+            display.DrawNoiseMap(noiseMap);
+        }
+        else if(drawMode == DrawMode.colorMap)
+        {
+
+        }
     }
 
     private void OnValidate()
@@ -44,5 +74,13 @@ public class MapGenerator : MonoBehaviour
         {
             octaves = 0;
         }
+    }
+
+    [System.Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color color;
     }
 }
