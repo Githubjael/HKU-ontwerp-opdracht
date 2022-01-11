5ee90b6fe2f9f470e90f99abb641795f7b330360
Githubjael <jaelimamdi@gmail.com>
2022-01-11 22:02:03 +0100

    Update MapGenerator.cs
    een instance binnen OnValidate functie gemaakt

--- Ontwerp-Opdracht/Assets/Scripts/MapGenerator.cs
+++ Ontwerp-Opdracht/Assets/Scripts/MapGenerator.cs
@@ -172,6 +172,8 @@ public class MapGenerator : MonoBehaviour
         {
             octaves = 0;
         }
+
+        fallOffMap = FallOffGenerator.GenerateFallOffMap(mapChunkSize);
     }
 
     struct MapThreadInfo<T>
