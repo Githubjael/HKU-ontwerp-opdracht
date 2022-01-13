a44a48a23aa9e05d74437e53a74efb0830165f0f
Githubjael <jaelimamdi@gmail.com>
2022-01-13 21:07:04 +0100

    dingen
    meshcollider toegevoegd bij endlessterrain en Hide on play script gemaakt, om een gameobject te schuilen op play.

--- Ontwerp-Opdracht/Assets/Scripts/EndlessTerrain.cs
+++ Ontwerp-Opdracht/Assets/Scripts/EndlessTerrain.cs
@@ -87,6 +87,7 @@ public class EndlessTerrain : MonoBehaviour
 
         MeshRenderer meshRenderer;
         MeshFilter meshFilter;
+        MeshCollider meshCollider;
 
         LODInfo[] detailLevels;
         LODMesh[] lodMeshes;
@@ -101,6 +102,7 @@ public class EndlessTerrain : MonoBehaviour
             meshObject = new GameObject("Terrain Chunk");
             meshRenderer = meshObject.AddComponent<MeshRenderer>();
             meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshCollider = meshObject.AddComponent<MeshCollider>();
 
             meshRenderer.material = material;
             meshObject.transform.position = positionV3 * scale;
@@ -160,6 +162,7 @@ public class EndlessTerrain : MonoBehaviour
                         {
                             previousLODIndex = lodIndex;
                             meshFilter.mesh = lodMesh.mesh;
+                            meshCollider.sharedMesh = lodMesh.mesh;
                         }
                         else if (!lodMesh.hasRequestedMesh)
                         {
