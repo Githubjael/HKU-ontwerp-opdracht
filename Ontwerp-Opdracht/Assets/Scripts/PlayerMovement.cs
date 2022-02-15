9d9b14494a6e98b9bf0b549768bdf0862d38ac7f
Githubjael <jaelimamdi@gmail.com>
2022-02-15 23:02:21 +0100

    compilers en Playermovement script

--- Ontwerp-Opdracht/Assets/Scripts/PlayerMovement.cs
+++ Ontwerp-Opdracht/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public CharacterController controller;
     public Joystick joystickController;
-    public GameObject Player;
+    public GameObject player;
+
+    public float speed = 6f;
+
+    public void Update()
+    {
+        float horizontal = joystickController.Horizontal;
+        float vertical = joystickController.Vertical;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
 }
