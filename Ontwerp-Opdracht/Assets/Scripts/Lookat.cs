e46a2be5d1279db74f3b60a05d0d0e90b4cf561a
Githubjael <jaelimamdi@gmail.com>
2022-02-15 16:45:44 +0100

    Joystick pack plus script
    ik heb een package gedownload en een script gemaakt om de camera de speler te volgen.

--- Ontwerp-Opdracht/Assets/Scripts/Lookat.cs
+++ Ontwerp-Opdracht/Assets/Scripts/Lookat.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Lookat : MonoBehaviour
 {
     public Transform LookatObject;
+    public float OffsetX;
+    public float OffsetY;
+    public float OffsetZ;
 
-    public void Start()
+    public void Update()
     {
-        transform.LookAt(LookatObject);
+        Follow();
+        gameObject.transform.LookAt(LookatObject);
+    }
+
+    public void Follow()
+    {
+        var Xposition = gameObject.transform.position.x + OffsetX;
+        var Yposition = gameObject.transform.position.y + OffsetX;
+        var Zposition = gameObject.transform.position.z + OffsetX;
+        gameObject.transform.position = new Vector3(Xposition, Yposition, Zposition);
     }
 }
